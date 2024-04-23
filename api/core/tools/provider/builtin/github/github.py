1d05bb162e7888f644b6fd9add164970ae1fa906
Richards Tu <142148415+richards199999@users.noreply.github.com>
2024-04-23 15:43:29 +0800

    Replace original GitHub with the new version

--- api/core/tools/provider/builtin/github/github.py
+++ api/core/tools/provider/builtin/github/github.py
@@ -1,32 +1,17 @@
-import requests
-
 from core.tools.errors import ToolProviderCredentialValidationError
+from core.tools.provider.builtin.github.tools.searchRepos import SearchReposTool
 from core.tools.provider.builtin_tool_provider import BuiltinToolProviderController
 
 
-class GihubProvider(BuiltinToolProviderController):
+class GithubProvider(BuiltinToolProviderController):
     def _validate_credentials(self, credentials: dict) -> None:
         try:
-            if 'access_tokens' not in credentials or not credentials.get('access_tokens'):
-                raise ToolProviderCredentialValidationError("Github API Access Tokens is required.")
-            if 'api_version' not in credentials or not credentials.get('api_version'):
-                api_version = '2022-11-28'
-            else:
-                api_version = credentials.get('api_version')
-
-            try:
-                headers = {
-                    "Content-Type": "application/vnd.github+json",
-                    "Authorization": f"Bearer {credentials.get('access_tokens')}",
-                    "X-GitHub-Api-Version": api_version
-                }
-
-                response = requests.get(
-                    url="https://api.github.com/search/users?q={account}".format(account='charli117'),
-                    headers=headers)
-                if response.status_code != 200:
-                    raise ToolProviderCredentialValidationError((response.json()).get('message'))
-            except Exception as e:
-                raise ToolProviderCredentialValidationError("Github API Key and Api Version is invalid. {}".format(e))
+            # Example of invoking a tool with credentials to validate them
+            SearchReposTool().fork_tool_runtime(
+                meta={"credentials": credentials}
+            ).invoke(
+                user_id='',
+                tool_parameters={"q": "test"}
+            )
         except Exception as e:
             raise ToolProviderCredentialValidationError(str(e))
