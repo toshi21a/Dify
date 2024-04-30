396e40cdf3e16bbecc86fab0e81e4e5f4be30f99
Bowen Liang <liangbowen@gf.com.cn>
2024-04-30 22:14:16 +0800

    update

---
+++ api/tests/unit_tests/utils/position_helper/test_position_helper.py
@@ -0,0 +1,29 @@
+import pytest
+
+from core.utils.position_helper import get_position_map
+
+
+@pytest.fixture
+def prepare_example_positions_yaml(tmp_path, monkeypatch) -> str:
+    monkeypatch.chdir(tmp_path)
+    tmp_path.joinpath("example_positions.yaml").write_text("""
+- first
+- second
+# - commented
+- third
+
+- forth
+            """)
+    return str(tmp_path)
+
+
+def test_position_helper(prepare_example_positions_yaml):
+    position_map = get_position_map(
+        folder_path=prepare_example_positions_yaml,
+        file_name='example_positions.yaml')
+    assert position_map == {
+        'first': 0,
+        'second': 1,
+        'third': 2,
+        'forth': 3,
+    }
